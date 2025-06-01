ea13e26a1dc3c2a108cdc4dccae1f435b5b6a8fe
Victor Abiscula <victorabiscula@live.com>
2025-05-31 21:23:52 -0300

    test: Adicionada AbstractIntegrationTest para criação do testcontainer
    Agora AutenticacaoControllerIT extends AbstractIntegrationTest

--- src/test/java/med/voll/api/integration/controller/AutenticacaoControllerIT.java
+++ src/test/java/med/voll/api/integration/controller/AutenticacaoControllerIT.java
@@ -4,38 +4,18 @@ import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.UsuarioRepository;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.integration.AbstractIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AutenticacaoControllerIT {
-
-    @Container
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("vollmed")
-            .withUsername("root")
-            .withPassword("root");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
+class AutenticacaoControllerIT extends AbstractIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
